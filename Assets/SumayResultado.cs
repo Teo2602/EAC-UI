6da3fa0f8f875fbcec077566ac0388a5ff071673
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2025-05-07 16:01:01 -0300

    Initial commit

---
+++ Assets/SumayResultado.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SumayResultado : MonoBehaviour
+{
+    public TMP_InputField Num1;
+    public TMP_InputField Num2;
+    public TextMeshProUGUI txtmensage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtmensage.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Sumar()
+    {
+        int num1 = int.Parse(Num1.text);
+        int num2 = int.Parse(Num2.text);
+        Debug.Log("sumar");
+        if (num2 == 0)
+        {
+            txtmensage.text = "El número dos no puede ser 0";
+            return;
+        }
+        else{
+            txtmensage.text = $"{num1}/{num2}={num1 / num2}";
+        }
+    }
+}
