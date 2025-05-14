59783fbb13c2f7b9a39e2b6be0948122a21753b2
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2025-05-14 15:37:13 -0300

    parcero

---
+++ Assets/Parificador.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Parificador : MonoBehaviour
+{
+    public TMP_InputField Num1;
+    public TextMeshProUGUI mensage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mensage.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ispar()
+    {
+        int num1 = int.Parse(Num1.text);
+        int resto = num1 % 2;
+        if (num1 < 0)
+        {
+            mensage.text = "el número debe ser mayor que 0";
+            return;
+        }
+        if (resto == 0)
+        {
+            mensage.text = "Tu número es par";
+        } else
+        {
+            mensage.text = "Tu número no es par";
+        }
+    }
+}
